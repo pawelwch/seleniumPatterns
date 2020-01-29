b3e453579e1952278ff62a87227f454dfa57ad05
pawelwch <pawelmaryniak@wp.pl>
2020-01-29 23:03:18 +0100

    refreshing the project

--- src/test/java/navigation/ApplicationURLs.java
+++
@@ -1,8 +0,0 @@
-package navigation;
-
-import configuration.AppProperties;
-
-public class ApplicationURLs {
-    public static final String APPLICATION_URL = AppProperties.getAllUrl();
-    public static final String LOGIN_URL = APPLICATION_URL + "actions/Account.action?signonForm=";
-}
